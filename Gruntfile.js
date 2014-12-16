e2be05f845a4bdbda7bd5a1683dc2e1182013ecf
Lars Wiedemann <Larswiedemann@gmx.net>
2014-12-16 20:23:02 +0100

    single for all Jasmine Tests

--- Gruntfile.js
+++ Gruntfile.js
@@ -369,7 +369,7 @@ module.exports = function (grunt) {
         karma: {
             unit: {
                 configFile: 'test/karma.conf.js',
-                singleRun: false
+                singleRun: true
             }
         },
         ngdocs: {
