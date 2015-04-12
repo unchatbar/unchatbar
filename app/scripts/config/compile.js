c1a9efba02f6a91c935b83ca7e461fea58c8ea12
gernsdorfer <larswiedemann@gmx.net>
2015-04-12 22:52:56 +0200

    fix unsafe blob link

---
+++ app/scripts/config/compile.js
@@ -0,0 +1,6 @@
+'use strict';
+
+angular.module('unchatbar').config(['$compileProvider', function ($compileProvider) {
+    $compileProvider.aHrefSanitizationWhitelist(/^\s*(https?|ftp|mailto|file|blob):/);
+}
+]);
