dd67f85729eb07cb9bd1b2728b89e255eb96154a
gernsdorfer <larswiedemann@gmx.net>
2015-04-17 13:13:18 +0200

    Update server.js

--- app/scripts/config/server.js
+++ app/scripts/config/server.js
@@ -23,33 +23,33 @@ angular.module('unchatbar')
                 });
             });
             BrokerProvider.addIceServer({
-                url:"turn:192.155.86.24:3478",
+                url:"turn:'192.155.86.24:3478'",
                 username:"easyRTC",
                 credential:"easyRTC@pass"
             });
             BrokerProvider.addIceServer({
-               "url": "turn:numb.viagenie.ca",
+               "url": "turn:'numb.viagenie.ca'",
                 "credential": "muazkh",
                 "username": "webrtc@live.com"
             });
             
               BrokerProvider.addIceServer({
                "username":"e7db750a-2fcc-40c6-8415-cab22743a68a",
-                "url":"turn:turn1.xirsys.com:443?transport=udp",
+                "url":"turn:'turn1.xirsys.com:443?transport=udp'",
                 "credential": "287ae254-9380-4f81-af88-e1cc9ed27eb0"
             });
             BrokerProvider.addIceServer({
                 "username":"e7db750a-2fcc-40c6-8415-cab22743a68a",
-                "url":"turn:turn1.xirsys.com:443?transport=tcp",
+                "url":"turn:'turn1.xirsys.com:443?transport=tcp'",
                 "credential": "287ae254-9380-4f81-af88-e1cc9ed27eb0"
             });
             BrokerProvider.addIceServer({
-                "url": "turn:192.158.29.39:3478?transport=udp",
+                "url": "turn:'192.158.29.39:3478?transport=udp'",
                 "credential": "JZEOEt2V3Qb0y27GRntt2u2PAYA=",
                 "username": "28224511:1379330808"
             });
             BrokerProvider.addIceServer({
-                "url": "turn:192.158.29.39:3478?transport=tcp",
+                "url": "turn:'192.158.29.39:3478?transport=tcp'",
                 "credential": "JZEOEt2V3Qb0y27GRntt2u2PAYA=",
                 "username": "28224511:1379330808"
             });
